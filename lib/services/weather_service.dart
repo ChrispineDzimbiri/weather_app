bfcb5109e8987e2611520932260f53235b4a2202
xophr03 <Anthwachinod@gmail.com>
2025-04-14 22:38:43 +0200

    imported weather service files

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,3 +2,5 @@ import 'dart:convert';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/models/weather_model.dart';
+
+import 'package:http/http.dart' as http;
