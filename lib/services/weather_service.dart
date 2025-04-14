eb960d60d508aec1415728f8e4592f1a4f535e8e
akblue4 <adeleedwin3@gmail.com>
2025-04-14 23:02:26 +0200

    handling api

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,5 +6,7 @@ import 'package:weather_app/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService{
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
+  final String apiKey;
 }
