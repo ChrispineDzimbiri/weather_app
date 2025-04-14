b8ed9b216e4f3f2a3af246ebc089e710aedf455a
akblue4 <adeleedwin3@gmail.com>
2025-04-14 23:05:50 +0200

    fetching weather data

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -9,4 +9,9 @@ class WeatherService{
   static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
   final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async{
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
 }
