6e1adad61971a96ddb0f1e7072ddfb7e2997a165
xophr03 <Anthwachinod@gmail.com>
2025-04-14 22:40:32 +0200

    imported weather pgae files

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../models/weather_model.dart';
+import '../services/weather_service.dart';
+import 'package:intl/intl.dart';
