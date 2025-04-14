4b08387bb19b1ed8ad37a947cc3744a35664a441
akblue4 <adeleedwin3@gmail.com>
2025-04-14 23:11:04 +0200

    decoding json

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -14,4 +14,9 @@ class WeatherService{
 
   Future<Weather> getWeather(String cityName) async{
     final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
-}
+
+    if (response.statusCode == 200){
+      return Weather.fromJson(jsonDecode(response.body));
+    }
+
+  }
