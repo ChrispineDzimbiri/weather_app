be936b68bb6f32e261b4f682cdc40ca7c9068cbf
OscarLipenga <bright7677lipenga@gmail.com>
2025-04-17 00:00:13 +0200

    added materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,15 @@ void main() async{
    WidgetsFlutterBinding.ensureInitialized();
    runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: WeatherPage(),
+    );
+  }
+}
