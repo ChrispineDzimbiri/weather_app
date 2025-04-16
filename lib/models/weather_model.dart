3b01344607641ceef1170e897794b184494e08ec
akblue4 <adeleedwin3@gmail.com>
2025-04-17 00:05:19 +0200

    updated the model file

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -7,5 +7,11 @@ Weather({required this.cityName,
   required this.temperature,
   required this.mainCondition
 });
-
+factory Weather.fromJson(Map<String, dynamic> json){
+  return Weather(
+    cityName: json['name'],
+    temperature: json['main']['temp'].toDouble(),
+    mainCondition: json['weather'][0]['main'],
+  );
+}
 }
