939661247e1f0a8612e5a77055baf6c698fc4f0b
akblue4 <adeleedwin3@gmail.com>
2025-04-16 23:49:32 +0200

    created wether class

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -3,15 +3,5 @@ class Weather{
   final double temperature;
   final String mainCondition;
 
-  Weather({required this.cityName,
-    required this.temperature,
-    required this.mainCondition
-  });
-  factory Weather.fromJson(Map<String, dynamic> json){
-    return Weather(
-      cityName: json['name'],
-      temperature: json['main']['temp'].toDouble(),
-      mainCondition: json['weather'][0]['main'],
-    );
-  }
+
 }
