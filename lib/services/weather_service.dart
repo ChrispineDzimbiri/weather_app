d03c7e912e3a7d91bb39859d1ad5e3f6cf607321
chrispine <chrispinedzimbiri@gmail.com>
2025-04-17 12:16:39 +0200

    added a getweather function to get weather by city name

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -13,4 +13,14 @@ class WeatherService{
 
   WeatherService(this.apiKey);
 
+  Future<Weather> getWeather(String cityName) async{
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200){
+      return Weather.fromJson(jsonDecode(response.body));
+    }else{
+      throw Exception('Weather data failed to load.');
+    }
+  }
+
 }
