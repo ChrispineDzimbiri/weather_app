fda0013647e07c5d3bbd4cac87a469fa45b80a5d
AliceJicks <alicemainala@gmail.com>
2025-04-17 23:43:38 +0200

    added switch statement

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -8,4 +8,26 @@ import 'package:intl/intl.dart';
 
 String getWeatherAnimation(String? condition) {
   if (condition == null) return 'assets/sunny.json';
+  switch (condition.toLowerCase()) {
+    case 'clouds':
+    case 'mist':
+    case 'smoke':
+    case 'haze':
+    case 'dust':
+    case 'fog':
+      return 'assets/cloudy.json';
+    case 'rain':
+    case 'drizzle':
+    case 'shower rain':
+      return 'assets/rain.json';
+    case 'thunderstorm':
+      return 'assets/thunder.json';
+    case 'clear':
+      return 'assets/sunny.json';
+    default:
+      return 'assets/sunny.json';
+  }
+}
+
+
 
