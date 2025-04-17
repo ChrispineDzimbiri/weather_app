7d6cc87b9c0ce85b446061ad4a14ceca773d9f62
chrispine <chrispinedzimbiri@gmail.com>
2025-04-17 12:14:05 +0200

    API key initialization

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,4 +7,10 @@ import 'package:http/http.dart' as http;
 
 class WeatherService{
 
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
 }
