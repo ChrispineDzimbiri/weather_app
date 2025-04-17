f53e38092ad3489d36c6df51f257c0c8039b81e1
chrispine <chrispinedzimbiri@gmail.com>
2025-04-17 12:02:13 +0200

    importing packanges

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/models/weather_model.dart';
+
+import 'package:http/http.dart' as http;
