79d0a6954e137c5f009fc4bdcdff1246ca03dfb0
chrispine <chrispinedzimbiri@gmail.com>
2025-04-17 12:21:26 +0200

    getting curent location, reverse geocoding, extract city name

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -30,4 +30,19 @@ class WeatherService{
       permission = await Geolocator.requestPermission();
     }
 
+    //fetch current location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    //convert the location into a list of placemark object
+    List<Placemark> placemarks =
+    await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    //extract the city name from the first placemark
+
+    String? city =placemarks[0].locality;
+
+    return city ?? "";
+  }
+
 }
